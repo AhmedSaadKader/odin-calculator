986aab662b1c9a680016115b403f77bf825b2235
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-21 21:56:07 +0200

    basic HTML and basic operations

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
-console.log('hello')
+let firstNumber, lastNumber, firstResult, finalResult;
+const numberButtonsList = document.querySelectorAll('.number-button')
+
+const add = (a, b) => a + b
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operate =  (operator, a, b) => {
+    operator === '+' ? add(a, b):
+    operator === '-' ? subtract(a, b):
+    operator === '*' ? multiply(a, b):
+    operator === '/' ? divide (a, b): null
+}
+
+const getNumberInput = function (e) {
+    let inputNumber = e.currentTarget.innerText
+}
+
+numberButtonsList.forEach(button => button.addEventListener('click', (e) => getNumberInput(e)))
