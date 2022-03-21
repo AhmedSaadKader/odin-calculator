6f928035aa87f720d16a9d4573808372645b3cfc
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-21 23:28:38 +0200

    first operations done and displayed

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
-let firstNumber, lastNumber, firstResult, finalResult;
+let firstNumber, lastNumber, operator, firstResult, finalResult, result;
 const numberButtonsList = document.querySelectorAll('.number-button')
+const operatorButtonsList = document.querySelectorAll('.operator-button')
+const display = document.querySelector('#display')
+const upperDisplay = document.querySelector('#upper-display')
+const lowerDisplay = document.querySelector('#lower-display')
+const equalsSign = document.querySelector('#operator-equals')
 
-const add = (a, b) => a + b
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const add = (a, b) => result = a + b;
+const subtract = (a, b) => result = a - b;
+const multiply = (a, b) => result = a * b;
+const divide = (a, b) => result = a / b;
 
 const operate =  (operator, a, b) => {
     operator === '+' ? add(a, b):
@@ -14,7 +19,33 @@ const operate =  (operator, a, b) => {
 }
 
 const getNumberInput = function (e) {
-    let inputNumber = e.currentTarget.innerText
+    !operator ? (!firstNumber)          ? (firstNumber = "" + e.currentTarget.innerText,
+                                            lowerDisplay.innerText = firstNumber)                     :
+                (firstNumber.length < 8)? (firstNumber = firstNumber + e.currentTarget.innerText,
+                                            lowerDisplay.innerText = firstNumber)                     :
+                null:
+    operator  ? (!lastNumber)           ? (lastNumber = "" + e.currentTarget.innerText, 
+                                            lowerDisplay.innerText = lastNumber,
+                                            upperDisplay.innerText = firstNumber + ' ' + operator)          :
+                (lastNumber.length < 8) ? (lastNumber = lastNumber + e.currentTarget.innerText,
+                                            lowerDisplay.innerText = lastNumber,
+                                            upperDisplay.innerText = firstNumber + ' ' + operator)    :
+                null:
+    null; 
+}
+
+const operatorClick = (e) => {
+    operator = e.currentTarget.innerText;
+    upperDisplay.innerText = firstNumber + ' ' + operator;
+    lowerDisplay.innerText = '';
+}
+
+const equalClick = () => {
+    operate(operator, parseInt(firstNumber), parseInt(lastNumber));
+    lowerDisplay.innerText = result;
+    upperDisplay.innerText = `${firstNumber} ${operator} ${lastNumber}`
 }
 
 numberButtonsList.forEach(button => button.addEventListener('click', (e) => getNumberInput(e)))
+operatorButtonsList.forEach(button => button.addEventListener('click', (e) => operatorClick(e)))
+equalsSign.addEventListener('click', () => equalClick())
